613ff33049b7c3bbde191793c66f2bf3863b09da
sisiliu1204 <sisiliu1204@gmail.com>
2015-09-30 14:22:33 +0800

    普工

--- fis-conf.js
+++ fis-conf.js
@@ -5,3 +5,24 @@
 //        "cascade": true
 //    })
 //});
+
+
+fis.match('**.css', {
+    postpackager : fis.plugin('autoprefixer', {
+            //"browsers": ["Android >= 2.3", "ChromeAndroid > 1%", "iOS >= 4"],
+            "browsers": ['last 2 versions'],
+            "cascade": false
+    })
+});
+
+//fis.config.merge({
+//    settings : {
+//        postprocessor : {
+//            autoprefixer : {
+//                // detail config (https://github.com/postcss/autoprefixer#browsers)
+//                "browsers": ["Android >= 2.3", "ChromeAndroid > 1%", "iOS >= 4"],
+//                "cascade": true
+//            }
+//        }
+//    }
+//});
